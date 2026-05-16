b7041a1001eb7e89371e0446361182857406505a
Abdul khazmuddin <97663822+KHAZMUDDIN@users.noreply.github.com>
2026-05-16 08:55:51 +0530

    uploaded files

---
+++ app.py
@@ -0,0 +1,49 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+llm = ChatGroq(
+    model='llama-3.3-70b-versatile'
+)
+
+template = PromptTemplate(
+    template="""
+    Explain {topic} in a {way} way.
+    Keep the explanation {length}.
+    """,
+    input_variables=['topic', 'way', 'length']
+)
+
+st.header("TeachMeLLM")
+topic = st.text_input("Enter Topic: ", placeholder="Quantization in LLMs")
+way = st.selectbox(
+    "Select explanation style",
+    ["easy", "technical", "5 year old", "professional"]
+)
+length = st.selectbox(
+    "Enter the length of the answer e.g., short, medium, long etc.",
+    ["very short","short", "long", "very long"]
+)
+
+
+if st.button('Generate'):
+
+    if topic and way and length:
+
+        chain = template | llm
+
+        result = chain.invoke({
+            'topic': topic,
+            'way': way,
+            'length': length
+        })
+
+        st.write(result.content)
+
+    else:
+        st.warning("Please fill all fields")
+
+
